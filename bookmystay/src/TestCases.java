a7927ddfa33a77dd2eeaf4ecdf85660519c81844
ShubhiRustagi <rustagishubhi20@gmail.com>
2026-03-17 08:43:52 +0530

    UC2 COMPLETED

--- bookmystay/src/TestCases.java
+++ bookmystay/src/TestCases.java
@@ -1,19 +1,68 @@
 
-public class TestCases {
+abstract class Room {
+    private String type;
+    private int beds;
+    private double price;
 
+    // Constructor
+    public Room(String type, int beds, double price) {
+        this.type = type;
+        this.beds = beds;
+        this.price = price;
+    }
+
+    public void displayDetails() {
+        System.out.println("Room Type: " + type);
+        System.out.println("Beds: " + beds);
+        System.out.println("Price per night: ₹" + price);
+    }
+}
+
+class SingleRoom extends Room {
+    public SingleRoom() {
+        super("Single Room", 1, 2000);
+    }
+}
+
+class DoubleRoom extends Room {
+    public DoubleRoom() {
+        super("Double Room", 2, 3500);
+    }
+}
+
+class SuiteRoom extends Room {
+    public SuiteRoom() {
+        super("Suite Room", 3, 6000);
+    }
+}
+
+public class TestCases {
 
     public static void main(String[] args) {
 
-        // Display welcome message
-        System.out.println("======================================");
-        System.out.println("   Welcome to Book My Stay App");
-        System.out.println("   Hotel Booking System v1.0");
-        System.out.println("======================================");
+        Room single = new SingleRoom();
+        Room doubleRoom = new DoubleRoom();
+        Room suite = new SuiteRoom();
 
-        // Inform user that application has started
-        System.out.println("Application started successfully!");
+        // Static availability variables
+        int singleAvailable = 5;
+        int doubleAvailable = 3;
+        int suiteAvailable = 2;
 
-        // End message
-        System.out.println("Thank you for using Book My Stay.");
+        System.out.println("===== Room Availability =====\n");
+
+        single.displayDetails();
+        System.out.println("Available: " + singleAvailable);
+        System.out.println();
+
+        doubleRoom.displayDetails();
+        System.out.println("Available: " + doubleAvailable);
+        System.out.println();
+
+        suite.displayDetails();
+        System.out.println("Available: " + suiteAvailable);
+        System.out.println();
+
+        System.out.println("===== End of Listing =====");
     }
 }
